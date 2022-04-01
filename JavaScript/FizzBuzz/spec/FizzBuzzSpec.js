18cf6751f99fefce95fff7a1585c0d30ee544a26
Todd Flanders <toddfbass@gmail.com>
2022-04-01 12:29:35 -0400

    initial commit

---
+++ JavaScript/FizzBuzz/spec/FizzBuzzSpec.js
@@ -0,0 +1,12 @@
+describe("FizzBuzz Tests", function() {
+
+    beforeEach(function() {
+        // player = new Player();
+        // song = new Song();
+    });
+
+    it("should be able to play a Song", function() {
+        expect(1).toEqual(2);
+    });
+
+});
